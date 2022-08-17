ce21761041a979de7b0b4121b31eb5bc65dfc4b7
minseong <kdsvip5@naver.com>
2022-08-17 19:42:42 +0900

    initial commit

---
+++ jumun/src/main/java/com/mihak/jumun/JumunApplication.java
@@ -0,0 +1,13 @@
+package com.mihak.jumun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumunApplication.class, args);
+	}
+
+}
---
+++ jumun/src/test/java/com/mihak/jumun/JumunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mihak.jumun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
