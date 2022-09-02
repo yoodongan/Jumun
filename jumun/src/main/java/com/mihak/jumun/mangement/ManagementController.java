f7660e819a3ce594ae09d99100c005f57170e636
hongsik cho <hongsikcho@hongsikui-MacBookAir7.local>
2022-09-03 01:36:54 +0900

    주문 리스트까지 url맵핑

--- jumun/src/main/java/com/mihak/jumun/mangement/ManagementController.java
+++ jumun/src/main/java/com/mihak/jumun/mangement/ManagementController.java
@@ -1,15 +1,23 @@
 package com.mihak.jumun.mangement;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/management")
 public class ManagementController {
 
-    @GetMapping("/home")
-    public String managementHome(){
+    @GetMapping("/{storeSN}/admin/store/order")
+    public String orderHome(Model model , @PathVariable String storeSN){
+        model.addAttribute("storeSN" , storeSN);
         return "management/managementHome";
+    }
+
+    @GetMapping("/{storeSN}/admin/store/order/list")
+    public String orderList(Model model , @PathVariable String storeSN){
+        model.addAttribute("storeSN" , storeSN);
+        return "management/orderList";
     }
 }
