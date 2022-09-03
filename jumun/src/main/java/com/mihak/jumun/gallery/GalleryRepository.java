c7925296c7af55b5e802a54f67d95a3f08d36d8f
Sapjil <53210680+castlehyeon@users.noreply.github.com>
2022-09-03 10:07:08 +0900

    Delete GalleryRepository.java
    S3작업을 위한 테스트 gallery폴더 삭제
    
    Signed-off-by: Sapjil <53210680+castlehyeon@users.noreply.github.com>

--- jumun/src/main/java/com/mihak/jumun/gallery/GalleryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mihak.jumun.gallery;
-
-import com.mihak.jumun.entity.Menu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GalleryRepository extends JpaRepository<Menu, Long> {
-}
