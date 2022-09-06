d078dd9f473de5fc8928d6a9d4f1fed9e0157ca9
minseong <kdsvip5@naver.com>
2022-09-06 16:47:31 +0900

    Feat : 장바구니 조회 기능 개발 (#23)
    - 장바구니 조회 뷰 생성 및 Dto 추가
    - CartMenuOption Entity, Service, Repository 수정
    - Cart Controller, Service, Repository 수정

--- jumun/src/main/java/com/mihak/jumun/cart/CartController.java
+++ jumun/src/main/java/com/mihak/jumun/cart/CartController.java
@@ -1,5 +1,6 @@
 package com.mihak.jumun.cart;
 
+import com.mihak.jumun.cart.dto.CartDto;
 import com.mihak.jumun.cart.dto.CartForm;
 import com.mihak.jumun.cartMenuOption.CartMenuOptionService;
 import com.mihak.jumun.entity.Cart;
@@ -7,10 +8,12 @@ import com.mihak.jumun.entity.Menu;
 import com.mihak.jumun.menu.MenuService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,6 +22,18 @@ public class CartController {
     private final CartService cartService;
     private final MenuService menuService;
     private final CartMenuOptionService cartMenuOptionService;
+
+    @GetMapping("{storeSN}/cart")
+    public String showCart(@PathVariable String storeSN, Model model,
+                           HttpServletRequest request, @CookieValue("customerLogin") String customerKey) {
+
+        HttpSession session = request.getSession(true);
+        String userNickname = session.getAttribute(customerKey).toString();
+
+        List<CartDto> cartDtoList = cartService.getCartByUserNickName(userNickname, false);
+        model.addAttribute("cartList", cartDtoList);
+        return "cart/cart_list";
+    }
 
     @PostMapping("{storeSN}/menu/{menuId}")
     public String saveCart(@PathVariable String storeSN,
@@ -35,4 +50,15 @@ public class CartController {
 
         return "redirect:/" + storeSN + "/menu";
     }
+
+//    @GetMapping("{storeSN}/menu/modify/{cartId}")
+//    public String modifyCart(@PathVariable String storeSN, @PathVariable String cartId,
+//                             @ModelAttribute CartForm cartForm, HttpServletRequest request,
+//                             @CookieValue("customerLogin") String customerKey) {
+//
+//        HttpSession session = request.getSession(true);
+//        String userNickname = session.getAttribute(customerKey).toString();
+//
+//
+//    }
 }
---
+++ jumun/src/main/java/com/mihak/jumun/exception/CartNotFoundException.java
@@ -0,0 +1,8 @@
+package com.mihak.jumun.exception;
+
+public class CartNotFoundException extends RuntimeException {
+
+    public CartNotFoundException(String message) {
+        super(message);
+    }
+}
