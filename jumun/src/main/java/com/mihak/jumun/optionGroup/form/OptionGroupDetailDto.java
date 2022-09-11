61890456cbd3dc456bf39b3f9bed1c6350c8667a
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-09-11 12:02:01 +0900

    feat :  옵션 그룹 상세페이지 템플릿 구현 (#46)
    - 옵션 그룹 상세페이지에서 추가한 옵션들 볼 수 있도록 구현

---
+++ jumun/src/main/java/com/mihak/jumun/optionGroup/form/OptionGroupDetailDto.java
@@ -0,0 +1,15 @@
+package com.mihak.jumun.optionGroup.form;
+
+import com.mihak.jumun.entity.Option;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class OptionGroupDetailDto {
+    private Long optionId;
+
+
+}
