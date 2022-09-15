5e674392104e67564ec8de73d7894217f38b7026
minseong <kdsvip5@naver.com>
2022-09-16 05:47:14 +0900

    Feat : Redis 적용하여 주문 기능 개발
    - Redis를 통해 관리하는 OrderDao 추가
    - (기존방식) 장바구니 페이지 내 데이터를 Order 테이블에 저장 → 결제 페이지에서 Order 조회하고, 추가적으로 데이터 수정 후 저장
    - (개선방식) 장바구니 페이지 내 데이터를 Redis에 저장 → 결제 페이지에서 Redis에서 조회하고, 한번에 Order 객체 생성

--- jumun/src/main/java/com/mihak/jumun/order/OrderController.java
+++ jumun/src/main/java/com/mihak/jumun/order/OrderController.java
@@ -1,10 +1,8 @@
 package com.mihak.jumun.order;
 
-import com.mihak.jumun.cart.dto.CartFormDto;
-import com.mihak.jumun.entity.Order;
-import com.mihak.jumun.entity.OrderType;
 import com.mihak.jumun.entity.PayType;
 import com.mihak.jumun.entity.Store;
+import com.mihak.jumun.order.dao.OrderDao;
 import com.mihak.jumun.order.dto.OrderDtoFromCart;
 import com.mihak.jumun.order.dto.OrderFormDto;
 import com.mihak.jumun.store.StoreService;
@@ -22,6 +20,7 @@ public class OrderController {
 
     private final OrderService orderService;
     private final StoreService storeService;
+    private final OrderDao orderDao;
 
     @PostMapping("/{storeSN}/order")
     public String order(@PathVariable String storeSN,
@@ -33,8 +32,8 @@ public class OrderController {
         orderDtoFromCart.setUserNickName(userNickname);
         orderDtoFromCart.setStoreSerialNumber(storeSN);
 
-        Order order = orderService.saveOrderWithOrderDto(orderDtoFromCart);
-        return "redirect:/" + storeSN + "/order/" + order.getId();
+        orderDao.addOrderDtoFromCart(orderDtoFromCart, userNickname);
+        return "redirect:/" + storeSN + "/pay";
     }
 
     @ModelAttribute("payTypes")
@@ -42,27 +41,34 @@ public class OrderController {
         return PayType.values();
     }
 
-    @GetMapping("/{storeSN}/order/{orderId}")
-    public String showOrder(@PathVariable String storeSN, @PathVariable Long orderId,
+    @GetMapping("/{storeSN}/pay")
+    public String showOrder(@PathVariable String storeSN,
                             HttpServletRequest request, @CookieValue("customerLogin") String customerKey,
                             Model model) {
 
+        HttpSession session = request.getSession(true);
+        String userNickname = session.getAttribute(customerKey).toString();
         Store store = storeService.findBySerialNumber(storeSN);
-        Order order = orderService.findOrderById(orderId);
-        int totalPrice = order.getTotalPrice();
+
+        OrderDtoFromCart dto = orderDao.getOrderDtoFromCart(userNickname);
 
         model.addAttribute("orderFormDto", new OrderFormDto());
         model.addAttribute("storeName", store.getName());
-        model.addAttribute("totalPrice", totalPrice);
+        model.addAttribute("totalPrice", dto.getTotalPrice());
+
         return "order/orderForm";
     }
 
-    @PostMapping("/{storeSN}/order/{orderId}")
+    @PostMapping("/{storeSN}/pay")
     public String doOrder(@PathVariable String storeSN, @PathVariable Long orderId,
                             HttpServletRequest request, @CookieValue("customerLogin") String customerKey,
                             @ModelAttribute OrderFormDto orderFormDto) {
 
-        orderService.setOrderAboutPay(orderId, orderFormDto);
+        HttpSession session = request.getSession(true);
+        String userNickname = session.getAttribute(customerKey).toString();
+
+        OrderDtoFromCart orderDtoFromCart = orderDao.getOrderDtoFromCart(userNickname);
+        orderService.saveOrder(orderDtoFromCart, orderFormDto);
         return "order/orderForm"; // 간편결제!
     }
 
