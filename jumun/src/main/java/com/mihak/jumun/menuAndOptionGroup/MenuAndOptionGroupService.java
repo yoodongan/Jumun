ee7739869ab9b2924285d8f12d5853489ba0380a
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-09-18 12:02:09 +0900

    feat : 메뉴에 옵션그룹 추가하기(#50)
    - 메뉴 수정 시, 메뉴에 옵션 그룹 하나씩 추가하도록 구현
    - 메뉴 옵션그룹 정보는 중간테이블에서 관리

--- jumun/src/main/java/com/mihak/jumun/menuAndOptionGroup/MenuAndOptionGroupService.java
+++ jumun/src/main/java/com/mihak/jumun/menuAndOptionGroup/MenuAndOptionGroupService.java
@@ -1,9 +1,11 @@
 package com.mihak.jumun.menuAndOptionGroup;
 import com.mihak.jumun.entity.Menu;
 import com.mihak.jumun.entity.MenuAndOptionGroup;
+import com.mihak.jumun.entity.OptionGroup;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,4 +17,33 @@ public class MenuAndOptionGroupService {
     public List<MenuAndOptionGroup> getMenuAndOptionGroupByMenu(Menu menu) {
         return menuAndOptionGroupRepository.findByMenu(menu);
     }
+
+//    public void addMenuOptionGroups(Menu menu, OptionGroup... optionGroups) {
+//        MenuAndOptionGroup menuAndOptionGroup = new MenuAndOptionGroup();
+//        for (OptionGroup optionGroup : optionGroups) {
+//            menuAndOptionGroup.addMenuAndOptionGroup(menu, optionGroup);
+//            menuAndOptionGroupRepository.save(menuAndOptionGroup);
+//        }
+//    }
+
+    public void addMenuOptionGroup(Menu menu, OptionGroup optionGroup) {
+        MenuAndOptionGroup menuAndOptionGroup = new MenuAndOptionGroup();
+        menuAndOptionGroup.addMenuAndOptionGroup(menu, optionGroup);
+        menuAndOptionGroupRepository.save(menuAndOptionGroup);
+    }
+
+
+    public List<MenuAndOptionGroup> findAllByMenu(Menu menu) {
+        return menuAndOptionGroupRepository.findByMenu(menu);
+    }
+
+    public List<OptionGroup> getOptionGroupsByMenu(Menu menu) {
+        List<MenuAndOptionGroup> allByMenu = findAllByMenu(menu);
+        List<OptionGroup> optionGroups = new ArrayList<>();
+        for (MenuAndOptionGroup menuAndOptionGroup : allByMenu) {
+            optionGroups.add(menuAndOptionGroup.getOptionGroup());
+        }
+        return optionGroups;
+    }
+
 }
