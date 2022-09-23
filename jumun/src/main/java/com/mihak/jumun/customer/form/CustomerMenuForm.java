cfc4924e2d0457baccddf5af3382f80b3d13822d
hongsikcho <jjdd9758@naver.com>
2022-09-23 16:02:51 +0900

    Feat: 카테고리 리스트에서 카테고리 추가버튼 생성

--- jumun/src/main/java/com/mihak/jumun/customer/form/CustomerMenuForm.java
+++ jumun/src/main/java/com/mihak/jumun/customer/form/CustomerMenuForm.java
@@ -19,9 +19,6 @@ public class CustomerMenuForm {
     private String description;
     private int price;
     private List<OptionGroup> optionGroups;
-//    private List<OptionAndOptionGroup> optionAndOptionGroupList;
     private List<Option> checkOptions;
     private int count;
-    //카운트는 위아래 화살표로 디폴트 값 1로?
-    //주문하기를 누르면 카트폼DTO로 넘어가야함.
 }
--- jumun/src/main/java/com/mihak/jumun/order/OrderRepository.java
+++ jumun/src/main/java/com/mihak/jumun/order/OrderRepository.java
@@ -1,8 +1,10 @@
 package com.mihak.jumun.order;
 
 import com.mihak.jumun.entity.Order;
+import com.mihak.jumun.entity.Store;
 import com.mihak.jumun.storeMgmt.dto.FindByUserDailyDto;
 import com.mihak.jumun.storeMgmt.dto.FindListFormDto;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,10 +15,10 @@ import java.util.Optional;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     // 매출별 일간쿼리. JPQL에서 지원하지 않는 date함수
-    @Query(value = "SELECT new com.mihak.jumun.storeMgmt.dto.FindListFormDto(o.orderedAt, sum(o.totalPrice))FROM Order o GROUP BY o.orderedAt")
-    List<FindListFormDto> findByPriceDaily();
+    @Query(value = "SELECT new com.mihak.jumun.storeMgmt.dto.FindListFormDto(o.orderedAt, sum(o.totalPrice))FROM Order o WHERE o.storeSerialNumber = :storeSN GROUP BY o.orderedAt")
+    List<FindListFormDto> findByPriceDaily(@Param("storeSN") String storeSN);
     // 이용자별 일간쿼리
-    @Query(value = "SELECT new com.mihak.jumun.storeMgmt.dto.FindByUserDailyDto(o.orderedAt, count(o.userNickName))FROM Order o GROUP BY o.orderedAt")
-    List<FindByUserDailyDto> findByUserDaily();
+    @Query(value = "SELECT new com.mihak.jumun.storeMgmt.dto.FindByUserDailyDto(o.orderedAt, count(o.userNickName))FROM Order o WHERE o.storeSerialNumber = :storeSN GROUP BY o.orderedAt")
+    List<FindByUserDailyDto> findByUserDaily(@Param("storeSN") String storeSN);
     Optional<Order> findByUserNickName(String userNickName);
 }
