7c5bebbba14ee8713f01c62069a0c6701cd6192a
yoodongan <dbehddks519@naver.com>
2023-02-27 11:19:40 +0900

    Feat : 옵션그룹, 옵션 중간 테이블 페치 조인 적용

--- jumun/src/main/java/com/mihak/jumun/optionAndOptionGroup/repository/OptionAndOptionGroupRepository.java
+++ jumun/src/main/java/com/mihak/jumun/optionAndOptionGroup/repository/OptionAndOptionGroupRepository.java
@@ -3,6 +3,7 @@ import com.mihak.jumun.option.entity.Option;
 import com.mihak.jumun.optionAndOptionGroup.entity.OptionAndOptionGroup;
 import com.mihak.jumun.optionGroup.entity.OptionGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -18,4 +19,10 @@ public interface OptionAndOptionGroupRepository extends JpaRepository<OptionAndO
     void deleteAllByOptionGroup(OptionGroup optionGroup);
 
     Optional<OptionAndOptionGroup> findByOptionGroupAndOption(OptionGroup optionGroup, Option option);
+
+    @Query("select oog from OptionAndOptionGroup oog " +
+            "join fetch oog.optionGroup og " +
+            "join fetch oog.option o")
+    List<OptionAndOptionGroup> findAll();
+
 }
